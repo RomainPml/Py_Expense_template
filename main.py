2a4fe942a4ce797adb13fef709a5ee1a0835c516
Romain <romainpauml@gmail.com>
2023-09-20 21:45:33 +0200

    [ADD] status report

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ from PyInquirer import prompt
 from examples import custom_style_2
 from expense import new_expense
 from user import add_user
-
-def ask_option():
-    main_option = {
-        "type":"list",
-        "name":"main_options",
-        "message":"Expense Tracker v0.1",
-        "choices": ["New Expense","Show Status","New User"]
-    }
-    option = prompt(main_option)
-    if (option['main_options']) == "New Expense":
-        new_expense()
-        ask_option()
-    if (option['main_options']) == "New User":
-        add_user()
-        ask_option()
+from balance import distribute_expenses
 
 def main():
-    ask_option()
+    while True:
+        options = [
+            {
+                "type": "list",
+                "name": "menu",
+                "message": "Expense Tracker v0.1",
+                "choices": ["New Expense", "Show Status", "New User", "Quit"],
+            }
+        ]
+
+        choice = prompt(options)["menu"]
+
+        if choice == "New Expense":
+            new_expense()
+        elif choice == "Show Status":
+            status_report = distribute_expenses()
+            if status_report:
+                print("Status Report:")
+                for line in status_report:
+                    print(line)
+            else:
+                print("No expenses found.")
+        elif choice == "New User":
+            add_user()
+        elif choice == "Quit":
+            break
 
 main()
